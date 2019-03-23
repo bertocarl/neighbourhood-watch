04a96a8bfc896252a30b2d48509580b1bbc915fc
Albert Omware <aomware@gmail.com>
2019-03-23 10:01:26 +0300

    added the url in hood

--- hood/urls.py
+++ hood/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    # url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    url(r'^jet/', include('jet.urls', 'jet')),  
+    # url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')), 
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
+    url(r'^tinymce',include('tinymce.urls')),
 ]
