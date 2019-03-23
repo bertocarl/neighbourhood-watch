c2744bb55b0fdc14aeee45746e4acbe3393d19bc
Albert Omware <aomware@gmail.com>
2019-03-23 10:51:14 +0300

    added the urls file

--- hood/urls.py
+++ hood/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
     # url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
-    # url(r'^tinymce',include('tinymce.urls')),
+    url(r'^tinymce',include('tinymce.urls')),
 ]
