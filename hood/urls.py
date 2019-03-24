a1dfe7140a3b6455407c06a17f60e6a6df053bad
Albert Omware <aomware@gmail.com>
2019-03-24 17:13:11 +0300

    added index pictures for the blog and notifications

--- hood/urls.py
+++ hood/urls.py
@@ -1,12 +1,12 @@
 
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('neighbour.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
-    # url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
-
+    url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
     url(r'^tinymce',include('tinymce.urls')),
 ]
