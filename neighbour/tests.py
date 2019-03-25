45caf53ae20b2f9e165500dbae24599201967062
Albert Omware <aomware@gmail.com>
2019-03-25 20:22:21 +0300

    added test for neighbourhood

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.Karen = hood(hood='Karen')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Karen,hood))
+
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+
+    def test_save_method(self):
+        self.Karen.save_hood()
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood)>0)
+
+    def test_delete_method(self):
+        self.Karen.delete_neighbourhood('Karen')
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood)==0)
+
